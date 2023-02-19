5ba89c7b11821c9472c9378a1e2c02d1ec104966
MOOR AI <moorsearch@gmail.com>
2023-02-19 13:47:12 +0300

    Initial commit

---
+++ lib/src/main.dart
@@ -0,0 +1,94 @@
+void main() {
+  // var name = "Amr";
+  // var age = 27;
+  // print("your name as : $name and your name is :$age");
+
+  //Arithmetic Operators
+  // var x= 5;
+  // var y= 5;
+  // print(x+y);
+
+  // var x=4;
+  // var y=6;
+  // var z=10;
+
+  // if (x !=15) {
+  //   print("X is not equal to 15 ");
+  // }
+
+  // if (x ==4) {
+  //   print("X is equal to 4 ");
+  // }
+
+  //Unary Operator ++  ,  --
+
+  // var x=5;
+  // print(x++);
+  // print(x);
+  // print(x);
+  // print(--x);
+
+  //Assignment Operators +=  , -=
+
+  // var x=5;
+  // x = x + 1 ;
+  // print(x);
+  // x +=1 ;
+  // print(x);
+  // x -=1 ;
+  // print(x);
+
+  // Ternary Operators  ?  :  if statement in one line
+
+  // var x=2;
+  // var result = (x >5) ? "X is Above 5" :"X is Below 10";
+  // print(x);
+  // print(result);
+
+  // Relational Operator
+
+  // var x=3;
+  // if (x > 1) {
+  //   print("X is greater than 1");
+  // }
+  // else if (x==5) {
+  //   print("X is Equal 5");
+  // }
+  // else{
+  //   print("X is not equal to 5");
+  // }
+
+  // Switch Case for More Than If Conditions
+  // Switch Only - Switch - is  As
+  // var x=5;
+  // switch(x){
+  //   case 1 :
+  //     print("X is equal to 1");
+  //     break;
+  //   case 2 :
+  //     print("X is equal to 2");
+  //     break;
+  //   case 3 :
+  //     print("X is equal to 3");
+  //     break;
+  //   case 4 :
+  //     print("X is equal to 4");
+  //     break;
+  //   default:
+  //     {
+  //       print("X is not Valid Number");
+  //     }
+  // }
+
+  // var x=5;
+  // if (x is int) {
+  //   print("X is INT");
+  //
+  // }
+  // var x = 5;
+  // var y = 6;
+  // var z = 6;
+  // if (y == z  && x != z) {
+  //   print("Y is equal to Z");
+  // }
+}
